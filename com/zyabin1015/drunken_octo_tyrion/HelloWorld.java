32e4297715f8e87d78399384461cdbddb5f48aad
Andrew Zyabin <zyabin1015@users.noreply.github.com>
2015-01-08 15:55:52 +0400

    Add Java code

---
+++ com/zyabin1015/drunken_octo_tyrion/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.zyabin1015.drunken_octo_tyrion; 
+
+/** 
+ * Hello World in Java.
+ */
+
+public class HelloWorld {
+  public static void main( String args[] ) {
+    System.out.println( "Hello World!" );
+  }
+}
